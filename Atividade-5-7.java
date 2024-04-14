fd418ede25d57746e48c60107cf4ac3c27627f0b
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-04-13 21:47:34 -0300

    Create Atividade-5-7.java

---
+++ Atividade-5-7.java
@@ -0,0 +1,43 @@
+public class Produto {
+
+    private int codigo;
+    private String descricao;
+    private double preco;
+    private int quantidadeEmEstoque = 0;
+
+
+    public Produto(int codigo, String descricao, double preco, int quantidadeEmEstoque) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setAdicionarEstoque(int quantidade) {
+        quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+    }
+
+    public void setRemoverEstoque(int quantidade) {
+        if (quantidadeEmEstoque >= quantidade)
+        {quantidadeEmEstoque = quantidadeEmEstoque - quantidade;}
+        else {
+            System.out.println("ERRO! Estoque insuficiente");
+        }
+    }
+}
